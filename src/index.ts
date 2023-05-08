4fe19d00c37f0f336230f2d8bc1eb4311bc1d95e
kraibse <33869366+kraibse@users.noreply.github.com>
2023-05-08 22:42:40 +0200

    refactor(index.ts): remove unused imports and code, replace GameManager instance with empty object
    The imports for GameManager and AsteroidField were removed as they were not being used. The GameManager instance was replaced with an empty object to avoid errors.

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,7 @@
  * The code executes when loaded in a browser.
  */
 
-// import * as p5 from "p5/global";
-import { GameManager } from './gamemanager'
-import { AsteroidField } from "./scene";
-
-const viewWidth = window.innerWidth;
-const viewHeight = window.innerHeight;
-
-const gm: GameManager = new GameManager(viewWidth, viewHeight);
-gm.loadScene(AsteroidField);
-
-export const scene = (p: p5) => {
-    p.setup = () => {
-        gm.loadSettings();
-    }
-
-    p.draw = () => {
-        gm.draw();
-    }
-};
+const gm: any = {};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).gm = gm; // instead of casting window to any, you can extend the Window interface: https://stackoverflow.com/a/43513740/5433572
